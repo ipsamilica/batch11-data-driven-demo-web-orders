7593d0cf1eb33ced4623288df11949180e8bf692
TairovichJR <yokubov.omonjon@gmail.com>
2024-04-13 01:39:23 -0400

    fixed 11

--- src/main/java/com/academy/techcenture/driver/Driver.java
+++ src/main/java/com/academy/techcenture/driver/Driver.java
@@ -1,6 +1,5 @@
 package com.academy.techcenture.driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -18,11 +17,9 @@ public class Driver {
         if (driver == null){
             switch (browser){
                 case "chrome":
-                    WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                     break;
                 case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                     break;
                 case "edge":
